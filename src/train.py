f69e50863bd60f47d41e9f1c3ad0f6b0f6589694
rakhaalif <xpn9x123@gmail.com>
2025-09-01 23:45:38 +0700

    fix train.py

--- src/train.py
+++ src/train.py
@@ -5,15 +5,21 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
+
 def train(random_state: int = 42):
     data = load_iris()
     X_train, X_test, y_train, y_test = train_test_split(
-        data.data, data.target, test_size=0.2, random_state=random_state, stratify=data.target
+        data.data,
+        data.target,
+        test_size=0.2,
+        random_state=random_state,
+        stratify=data.target,
     )
     clf = LogisticRegression(max_iter=500)
     clf.fit(X_train, y_train)
     acc = accuracy_score(y_test, clf.predict(X_test))
     return clf, {"accuracy": float(acc)}
+
 
 if __name__ == "__main__":
     model, metrics = train()
